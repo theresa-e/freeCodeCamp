5047ff9937789b2bd4f624691d97f884d37c4e7d
Theresa Eliger <910theresa@gmail.com>
2018-05-13 23:23:04 -0700

    Adding palindrome algo

---
+++ Basic Algorithm Scripting/palindrome.js
@@ -0,0 +1,12 @@
+// Return true if the given string is a palindrome.Otherwise, return false.
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+function palindrome(str) {
+    var strReg = str.split('').reverse().join('').toLowerCase().replace(/[^a-z0-9+]+/gi, "");
+    var strPal = str.toLowerCase().replace(/[^a-z0-9+]+/gi, "");
+    if (strReg === strPal) {
+        return true;
+    } else {
+        return false;
+    }
+}
