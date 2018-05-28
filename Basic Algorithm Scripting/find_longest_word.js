59402bf5cfca8a9183c1e9f825ba4db521a64b1d
Theresa Eliger <910theresa@gmail.com>
2018-05-27 20:35:58 -0700

    Adding 'reverse a string.'

---
+++ Basic Algorithm Scripting/find_longest_word.js
@@ -0,0 +1,15 @@
+// Return the length of the longest word in the provided sentence.
+//
+// Your response should be a number.
+
+function findLongestWord(str) {
+  var splitStr = str.split(' ');
+  var longest = splitStr[0];
+  for (var i=0; i<splitStr.length; i++) {
+    if (splitStr[i].length > longest.length){
+      longest = splitStr[i];
+    }
+  }
+  return longest.length;
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
